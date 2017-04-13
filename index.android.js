06fa7653cef6cdc1fcef5848f3a298eea3432598
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-04-13 18:28:01 -0300

    modal working on scan

--- index.android.js
+++ index.android.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Modal,
   Dimensions,
   StyleSheet,
   Text,
@@ -11,9 +12,34 @@ import {
 import Camera from 'react-native-camera';
 
 class ScanApp extends Component {
+  state = {
+    modalVisible: false,
+  }
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {console.log("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22}}>
+          <View>
+            <Text>{codeId}</Text>
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>OK</Text>
+            </TouchableHighlight>
+
+          </View>
+         </View>
+        </Modal>
         <View style={styles.top}></View>
         <Camera
           ref={(cam) => {
@@ -28,12 +54,12 @@ class ScanApp extends Component {
     );
   }
   onBarCodeRead(e) {
-    alert(
-        e.data
-    );
+    codeId = e.data;
+    this.setModalVisible(true);
   }
 }
 
+const codeId = ':D';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
